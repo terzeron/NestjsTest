8be46d2d78f026fb01debb032a991e0c5b1354ba
Young-il Cho <youngilcho@nhnent.com>
2019-11-13 17:37:16 +0900

    add a cat controller/service example

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+    constructor(private readonly catsService: CatsService) {}
+
+    @Post()
+    async create(@Body() createCatDto: CreateCatDto) {
+        this.catsService.create(createCatDto);
+    }
+    // curl -X POST -d '{name: "Meow", age: 10, breed: "Wildcat"}' http://localhost:3000/cats
+
+    @Get()
+    async findAll(): Promise<Cat[]> {
+        return this.catsService.findAll();
+    }
+    // curl http://localhost:3000/cats
+}
