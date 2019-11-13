ce77b80c8a544ed9ada05c7ed851df7af5267f73
Young-il Cho <youngilcho@nhnent.com>
2019-11-13 17:23:37 +0900

    add some comments and example code

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get, Req, Post, HttpCode, Header, Param } from '@nestjs/common';
+import { Controller, Get, Req, Post, HttpCode, Header, Param, Query, Put, Body } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { UpdateCatDto } from './update-cat.dto';
 
 @Controller('cats')
 export class CatsController {
     @Post()
-    @Header('Cache-Control', 'none')
-    @HttpCode(204)
+    @Header('Cache-Control', 'none') // 응답 헤더
+    @HttpCode(204) // 응답 상태 코드
     create(): string {
         return 'This action adds a new cat (no action really)';
     }
@@ -17,6 +18,16 @@ export class CatsController {
     }
     // curl http://localhost:3000/cats
 
+    @Get()
+    findAll2(@Query() query) {
+        return 'This action returns all cats (limit: ${query.limit} items)';
+    }
+
+    @Put(':id')
+    update(@Param('id') id, @Body() updateCatDto: UpdateCatDto) {
+        return 'This action updates a #${id} cat';
+    }
+
     @Get('ab*cd')
     findAllWithWildCard() {
         return 'This route uses a wildcard';
@@ -24,16 +35,16 @@ export class CatsController {
     // curl http://localhost:3000/cats/ab111cd
 
     @Get(':id')
-    findOne(@Param('id') id): string {
+    findOne(@Param('id') id): string { // 단일 변수에 바인딩
         console.log(id);
-        return `This actions returns a # ${id} cat`;
+        return `This actions returns a #${id} cat`;
     }
     // curl http://localhost:3000/cats/400
     
     @Get('p/:id')
-    findOneWithParams(@Param() params): string {
+    findOneWithParams(@Param() params): string { // 객체 속성으로 바인딩
         console.log(params.id);
-        return `This actions returns a # ${params.id} cat`;
+        return `This actions returns a #${params.id} cat`;
     }
     // curl http://localhost:3000/cats/p/500
 }
--- src/dogs/dogs.controller.ts
+++ src/dogs/dogs.controller.ts
@@ -4,13 +4,13 @@ import { CreateDogDto } from './create-dog.dto';
 @Controller('dogs')
 export class DogsController {
     @Post()
-    async create(@Body() createDogDto: CreateDogDto) {
+    async create(@Body() createDogDto: CreateDogDto) { 
         return 'This action adds a new dog ' + JSON.stringify(createDogDto);
     }
     // curl -X POST -d '{name: "Bark", age: 10, breed: "Poodle" }' http://localhost:3000/dogs
     
     @Get()
-    async findAll(): Promise<any[]> {
+    async findAll(): Promise<any[]> { // promise를 반환
         return [1, 2, 3];
     }
     // curl http://localhost:3000/dogs
